ebdf0e553c2868ed6eebb0d21861c786636d368e
Nathan Denni <nathandenni@gmail.com>
2022-03-26 22:57:04 +0000

    add code in files types: .xrf remove code in files types: .java,.xrf

--- content.java
+++ content.java
@@ -3,8 +3,5 @@ public class Cqkseq {
         System.out.println("gjcqr");
         System.out.println("uhabs");
         System.out.println("pfkpr");
-        System.out.println("kqfyl");
-        System.out.println("mhlvs");
-        System.out.println("imvpu");
     }
 }
