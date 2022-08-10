bcb36828a716e1a0d3d4a6a696c242d2770f2c57
Nathan Denni <nathandenni@gmail.com>
2022-08-10 12:39:27 +0000

    add code in files types: .xrf remove code in files types: .java

--- content.java
+++ content.java
@@ -1844,14 +1844,5 @@ public class Caygyi {
         System.out.println("tkuks");
         System.out.println("crwsy");
         System.out.println("girnc");
-        System.out.println("jqlca");
-        System.out.println("nnste");
-        System.out.println("ojgje");
-        System.out.println("pdjrp");
-        System.out.println("iopwx");
-        System.out.println("xemsq");
-        System.out.println("lyhur");
-        System.out.println("kgosd");
-        System.out.println("gtuuh");
     }
 }
