4fbd3152a4672cb3b0b385b6b56ab5b09338141e
Nathan Denni <nathandenni@gmail.com>
2022-08-25 04:29:40 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2043,20 +2043,6 @@ public class Caygyi {
         System.out.println("foknb");
         System.out.println("ocrur");
         System.out.println("prgtt");
-        System.out.println("refcq");
-        System.out.println("tytet");
-        System.out.println("jvbmf");
-        System.out.println("fheuq");
-        System.out.println("hykxf");
-        System.out.println("gryjl");
-        System.out.println("sifpq");
-        System.out.println("ikqtr");
-        System.out.println("kyjsb");
-        System.out.println("fyesu");
-        System.out.println("mmknk");
-        System.out.println("wkbhp");
-        System.out.println("tpvql");
-        System.out.println("ldfbg");
-        System.out.println("govfm");
+        System.out.println("yqeys");
     }
 }
