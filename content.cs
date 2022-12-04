93ceffa433223600d7cd245cda882c39ff0e9ea2
Nathan Denni <nathandenni@gmail.com>
2022-12-04 19:56:46 +0000

    add code in files types: .cs remove code in files types: .cs

---
+++ content.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MockCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("auamk");
+            Console.WriteLine("gvmyg");
+            Console.WriteLine("kqyhl");
+        }
+    }
+}
