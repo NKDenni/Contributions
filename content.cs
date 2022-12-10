ec70d7049432c0fe92a12632c312cb6e72314de1
Nathan Denni <nathandenni@gmail.com>
2022-12-10 05:32:26 +0000

    add code in files types: .record,.cs remove code in files types:

--- content.cs
+++ content.cs
@@ -9,6 +9,20 @@ namespace MockCode
             Console.WriteLine("auamk");
             Console.WriteLine("gvmyg");
             Console.WriteLine("kqyhl");
+            Console.WriteLine("wivav");
+            Console.WriteLine("tgbpf");
+            Console.WriteLine("rtydi");
+            Console.WriteLine("vulxk");
+            Console.WriteLine("cirpo");
+            Console.WriteLine("pokjj");
+            Console.WriteLine("agyya");
+            Console.WriteLine("tvgsv");
+            Console.WriteLine("loipx");
+            Console.WriteLine("jtehw");
+            Console.WriteLine("yxxdu");
+            Console.WriteLine("eigwj");
+            Console.WriteLine("tdpvm");
+            Console.WriteLine("rcumv");
         }
     }
 }
