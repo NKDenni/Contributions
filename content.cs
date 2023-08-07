4de553e393ddebf29cde106dcd596fee636eae41
Nathan Denni <nathandenni@gmail.com>
2023-08-07 00:43:55 +0000

    add code in files types: .xrf remove code in files types: .cs

--- content.cs
+++ content.cs
@@ -608,18 +608,6 @@ namespace MockCode
             Console.WriteLine("ovdhd");
             Console.WriteLine("lntof");
             Console.WriteLine("detvi");
-            Console.WriteLine("qwjue");
-            Console.WriteLine("ofdxm");
-            Console.WriteLine("wnumu");
-            Console.WriteLine("ghkqw");
-            Console.WriteLine("tfpsx");
-            Console.WriteLine("dcoou");
-            Console.WriteLine("whcbd");
-            Console.WriteLine("balxb");
-            Console.WriteLine("iyiod");
-            Console.WriteLine("fmcuv");
-            Console.WriteLine("hpmis");
-            Console.WriteLine("bxwis");
         }
     }
 }
