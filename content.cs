50770ee10ada3f815468bb76c8d834466e3bf371
Nathan Denni <nathandenni@gmail.com>
2023-10-10 10:07:52 +0000

    add code in files types: .cs remove code in files types: .cs

--- content.cs
+++ content.cs
@@ -562,7 +562,7 @@ namespace MockCode
             Console.WriteLine("mcuur");
             Console.WriteLine("istpu");
             Console.WriteLine("hahuy");
-            Console.WriteLine("rmxjk");
+            Console.WriteLine("xyiou");
         }
     }
 }
