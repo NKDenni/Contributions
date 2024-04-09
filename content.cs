4e63aa83df6cfecea7a0565a5cc438d927624ee4
Nathan Denni <nathandenni@gmail.com>
2024-04-09 12:44:27 +0000

    add code in files types: .json remove code in files types: .cs,.record,.xrf

--- content.cs
+++ content.cs
@@ -1054,16 +1054,6 @@ namespace MockCode
             Console.WriteLine("qxxup");
             Console.WriteLine("qphbp");
             Console.WriteLine("usdxt");
-            Console.WriteLine("thokj");
-            Console.WriteLine("yowej");
-            Console.WriteLine("dnfol");
-            Console.WriteLine("lijjd");
-            Console.WriteLine("xpnmx");
-            Console.WriteLine("asaev");
-            Console.WriteLine("fesoh");
-            Console.WriteLine("xpmhs");
-            Console.WriteLine("faxil");
-            Console.WriteLine("xpciv");
         }
     }
 }
